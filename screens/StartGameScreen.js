15e8e8446bc4553b68572d7af04e15bc978b870d
David Obodo <obododavid5@gmail.com>
2020-06-01 13:43:42 +0100

    complete installing expo fonts

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -87,7 +87,8 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20,
-        marginVertical: 10
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold'
     },
     input: {
         width: 50,
