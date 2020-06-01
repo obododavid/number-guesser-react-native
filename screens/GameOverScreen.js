d7e97cdcbed425128af67822c712a6aead029b75
David Obodo <obododavid5@gmail.com>
2020-06-01 17:40:12 +0100

    adding images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import { StyleSheet, View, Button, Text, Image } from 'react-native';
+
+import DefautStyles from '../constants/default-styles';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was : {props.userNumber}</Text>
+            <Text style={DefautStyles.title}>The Game is Over</Text>
+            <View style={styles.imageContainer}>
+                <Image
+                    // source={require('../assets/success.png')}
+                    source={{ uri: 'https://images.unsplash.com/photo-1587613754760-cd9a285831b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' }}
+                    fadeDuration={1000}
+                    style={styles.image}
+                    resizeMode="cover" />
+            </View>
+            <Text style={DefautStyles.bodyText}>Number of rounds: {props.roundsNumber}</Text>
+            <Text style={DefautStyles.bodyText}>Number was : {props.userNumber}</Text>
             <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     )
@@ -17,6 +27,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 200
     }
 })
 
